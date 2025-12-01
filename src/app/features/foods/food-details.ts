07daa2b0f46c7fec29a69e2fc59b6c8159d1c21a
amenallah53 <amenkalai53@gmail.com>
2025-12-02 00:42:16 +0100

    start foods/[:id]

--- src/app/features/foods/food-details.ts
+++ src/app/features/foods/food-details.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { foods } from '../../shared/utils/foods';
+import { Food } from '../../shared/models/food';
+import { GalleriaModule } from 'primeng/galleria';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-food-details',
-  imports: [],
+  standalone: true,
   templateUrl: './food-details.html',
-  styleUrl: './foods.css',
+  styleUrls: ['./foods.css'],
+  imports: [CommonModule, NgIf, GalleriaModule, ButtonModule],
 })
 export class FoodDetails {
+  food?: Food;
 
+  constructor(private route: ActivatedRoute) {}
+
+  responsiveOptions: any[] = [
+      {
+          breakpoint: '1300px',
+          numVisible: 4
+      },
+      {
+          breakpoint: '575px',
+          numVisible: 1
+      }
+  ];
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('foodId');
+    if (!id) return;
+
+    this.food = foods.find(f => f.id === id);
+    console.log(this.food);
+  }
 }
--- src/app/shared/components/cards/food-card/food-card.ts
+++ src/app/shared/components/cards/food-card/food-card.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Food } from '../../../models/food';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-food-card',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './food-card.html',
   styleUrls: ['./food-card.css'],
 })
